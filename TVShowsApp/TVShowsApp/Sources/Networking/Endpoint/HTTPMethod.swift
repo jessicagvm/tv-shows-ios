dd93427813927e8180897cf5bfcb947b74aaaa23
Jessica Vasquez <jessicavasquezmontaguth@gmail.com>
2025-05-13 11:30:07 -0300

    [NETWORKING]: - add NetworkClient using async/await. - define NetworkClientProtocol and Endpoint protocol. - create ShowEndpoint with showList case. - create NetworkError with localized descriptions.

---
+++ TVShowsApp/TVShowsApp/Sources/Networking/Endpoint/HTTPMethod.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPMethod.swift
+//  TVShowsApp
+//
+//  Created by Jessica Vasquez on 13/05/2025.
+//
+
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
---
+++ TVShowsApp/TVShowsApp/Sources/Networking/Endpoint/ShowEndpoint.swift
@@ -0,0 +1,58 @@
+//
+//  ShowEndpoint.swift
+//  TVShowsApp
+//
+//  Created by Jessica Vasquez on 12/05/2025.
+//
+import Foundation
+
+enum ShowEndpoint {
+    case showList(page: String)
+}
+
+extension ShowEndpoint: Endpoint {
+    func getURLRequest() -> URLRequest? {
+        guard var components = URLComponents(string: baseURL + path) else {
+            return nil
+        }
+        
+        components.queryItems = queryItems
+        guard let url = components.url else {
+            return nil
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.httpBody = body
+        headers?.forEach { request.setValue($0.value, forHTTPHeaderField: $0.key) }
+        return request
+    }
+}
+
+private extension ShowEndpoint {
+    var baseURL: String { "https://api.tvmaze.com" }
+    
+    var path: String {
+        switch self {
+        case .showList(_):
+            return "/shows"
+        }
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var headers: [String: String]? {
+        ["Content-Type": "application/json"]
+    }
+    
+    var queryItems: [URLQueryItem]? {
+        switch self {
+        case .showList(let query):
+            return [URLQueryItem(name: "page", value: query)]
+        }
+    }
+    
+    var body: Data? { nil }
+}
